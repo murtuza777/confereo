4ae199e3232f08c6a3914ea5a61065c6d346f060
murtuza777 <mdmurtuzaali777@gmail.com>
2025-08-15 18:39:05 +0530

    feat(nav): Add navigation link constants and types

---
+++ constants/index.ts
@@ -0,0 +1,33 @@
+export interface SidebarLink {
+  label: string;
+  route: string;
+  imgUrl: string;
+}
+
+export const sidebarLinks: SidebarLink[] = [
+  {
+    label: 'Home',
+    route: '/',
+    imgUrl: '/icons/Home.svg',
+  },
+  {
+    label: 'Upcoming',
+    route: '/upcoming',
+    imgUrl: '/icons/upcoming.svg',
+  },
+  {
+    label: 'Previous',
+    route: '/previous',
+    imgUrl: '/icons/previous.svg',
+  },
+  {
+    label: 'Recordings',
+    route: '/recordings',
+    imgUrl: '/icons/Video.svg',
+  },
+  {
+    label: 'Personal-Room',
+    route: '/personal-room',
+    imgUrl: '/icons/add-personal.svg',
+  }
+];
