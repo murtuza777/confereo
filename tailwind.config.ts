13ccf69ccc35f2999f06ee0fa91591782d738353
murtuza777 <mdmurtuzaali777@gmail.com>
2025-10-10 21:05:52 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,45 +26,14 @@ const config: Config = {
         },
         blue: {
           1:'#0877FF'},
-        border: "var(--color-border)",
-        input: "var(--color-input)",
-        ring: "var(--color-ring)",
-        background: "var(--color-background)",
-        foreground: "var(--color-foreground)",
-        primary: {
-          DEFAULT: "var(--color-primary)",
-          foreground: "var(--color-primary-foreground)",
         },
-        secondary: {
-          DEFAULT: "var(--color-secondary)",
-          foreground: "var(--color-secondary-foreground)",
+
+        sky: {
+          1: '#C9DDFF'
         },
-        destructive: {
-          DEFAULT: "var(--color-destructive)",
+        backgroundImage: {
+          hero: "url('/images/hero-background.png')"
         },
-        muted: {
-          DEFAULT: "var(--color-muted)",
-          foreground: "var(--color-muted-foreground)",
-        },
-        accent: {
-          DEFAULT: "var(--color-accent)",
-          foreground: "var(--color-accent-foreground)",
-        },
-        popover: {
-          DEFAULT: "var(--color-popover)",
-          foreground: "var(--color-popover-foreground)",
-        },
-        card: {
-          DEFAULT: "var(--color-card)",
-          foreground: "var(--color-card-foreground)",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius-lg)",
-        md: "var(--radius-md)",
-        sm: "var(--radius-sm)",
-        xl: "var(--radius-xl)",
-      },
       fontFamily: {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-mono)"],
