e711fd2214d0606a0e753505219288ae84f28a3a
murtuza777 <mdmurtuzaali777@gmail.com>
2025-11-25 17:09:58 +0530

    CallList implt

--- components/ui/CallList.tsx
+++ components/ui/CallList.tsx
@@ -1,6 +1,43 @@
+'use client'
+
+import { useGetCalls } from '@/hooks/useGetCalls';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
-const CallList = () => {
+const CallList = ({type}: {type: 'ended' | 'recordings' | 'upcoming'}) => {
+
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls();
+  const router = useRouter();
+
+
+  const getCalls = () => {
+    switch (type) {
+      case 'ended':
+        return endedCalls;
+      case 'recordings':
+        return callRecordings;  
+      case 'upcoming':
+        return upcomingCalls;  
+      default:
+        return [];
+    }
+  }
+
+
+  
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case 'ended':
+        return 'NO previous Calls';
+      case 'recordings':
+        return 'No recording';  
+      case 'upcoming':
+        return 'No upcoming Calls';  
+      default:
+        return '';
+    }
+  }
+
   return (
     <div>CallList</div>
   )
