6358a6b7197813f4403375d62b7c681d7d521929
murtuza777 <mdmurtuzaali777@gmail.com>
2025-11-25 16:10:00 +0530

    useGetCalls

---
+++ hooks/useGetCalls.ts
@@ -0,0 +1,42 @@
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react"
+
+export const useGetCalls = () => {
+const [calls,setCalls] = useState<Call[]>([]);
+const [isLoading,setIsLoading] = useState(false);
+const client = useStreamVideoClient();
+const {user} = useUser();
+
+useEffect(() => {
+    const LoadCalls = async () => {
+        if (!client || !user) return;
+
+        setIsLoading(true);
+
+        try {
+            const {calls} = await client.queryCalls({
+                sort : [{field: 'starts_at', direction:-1}],
+                filter_conditions: {
+                    starts_at: {$exists: true},
+                    $or: [
+                        {created_by_user_id: user.id},
+                        {members: {$in: [user.id]}},
+                    ]
+                }
+            });
+
+            setCalls(calls);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+    }
+}
+
+LoadCalls();
+}, [client,user?.id])
+
+return calls;
+
+}
